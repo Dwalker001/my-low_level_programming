ff5fa3421fd60a0713fa52a037807e4aebe5c297
Dwalker001 <clintonakpan2019@gmail.com>
2022-08-05 10:08:40 -0700

    more and more basics

---
+++ C_if_else_SHORTHAND.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ * The if... else SHORTHAND further tells me
+ * rather, my code if one condition fails
+ * try the other
+ */
+
+int main()
+{
+    int seconds = 20;
+    if (seconds < 18){
+    printf("Its a damn shame.");}
+    else{
+    printf("Good! We are learning.");
+    }
+    return 0;
+}
+
