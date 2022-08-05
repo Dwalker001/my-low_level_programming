bbf8f9566a5981c8f8e38a12c08f65aa8ab0fb05
Dwalker001 <clintonakpan2019@gmail.com>
2022-08-05 10:09:47 -0700

    just another conditional

---
+++ C_if_else.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * if statements specifies a block of C code
+ * to be executed if the condition is true
+ */
+
+int main(){
+ int x = 50;
+ int y = 38;
+ if (x > y)
+ printf("x is greater than y");
+
+ return 0;
+}
+
