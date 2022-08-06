121addd252cd32b776a418146b8a93b2fd80ae41
Dwalker001 <clintonakpan2019@gmail.com>
2022-08-05 18:41:40 -0700

    C-strings

---
+++ strings.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * To output the string, you can use the printf()
+ * function together with the format specifier %s to tell C that
+ * we are now working with strings:
+ * use the %c format specifier to print a single character.
+ * use the %s format specifier to print the whole character.
+ */
+
+
+int main()
+
+{
+    char greetings[] = "Hello! World.";
+    printf("%s\n", greetings);
+    printf("%c\n", greetings[0]);
+    greetings[7] = 'M';
+    printf("%s\n", greetings);
+
+    char greetings1[] = {'H', 'e',  'l', 'l', 'o', ' ', 'W', 'o', 'r', 'l', 'd', '!', '\0'};
+    char greetings2[] = "Hello World!";
+
+    printf("%lu\n", sizeof(greetings1));
+    printf("%lu\n", sizeof(greetings2));
+    printf("%s\n", greetings1);
+
+
+
+}
+
+
