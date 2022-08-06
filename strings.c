b29e4cc0f0b84dafaeb90bfa5ee81ab658ccb361
Dwalker001 <clintonakpan2019@gmail.com>
2022-08-05 18:46:26 -0700

    Strings

--- strings.c
+++ strings.c
@@ -6,6 +6,8 @@
  * we are now working with strings:
  * use the %c format specifier to print a single character.
  * use the %s format specifier to print the whole character.
+ * when using the long method for printing a string,
+ * use the \0 character at the end to specify the end of the string.
  */
 
 
